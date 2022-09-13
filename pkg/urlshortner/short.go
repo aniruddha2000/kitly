3f9a3a8055c06fd09fbf5e233b569df1b42962a8
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-09-13 15:10:37 +0530

    Add all methods for file operations
    Signed-off-by: Aniruddha Basak <codewithaniruddha@gmail.com>

--- pkg/urlshortner/short.go
+++ pkg/urlshortner/short.go
@@ -1,41 +1,64 @@
 package urlshortner
 
-import b64 "encoding/base64"
-
-type Store struct {
-	Data map[string]string
-}
-
-func NewStore() *Store {
-	return &Store{Data: map[string]string{}}
-}
+import (
+	"bufio"
+	b64 "encoding/base64"
+	"log"
+	"os"
+	"strings"
+)
 
 type Shorter interface {
 	Short(string) string
 }
 
-func (s *Store) Short(url string) string {
-	val, ok := s.find(url)
+type FileStore struct {
+	File *os.File
+}
+
+func NewFileStore(f *os.File) *FileStore {
+	return &FileStore{File: f}
+}
+
+func (fs *FileStore) Short(url string) string {
+	log.Println("Short called")
+	val, ok := fs.find(url)
 	if ok {
 		return val
 	}
 
-	shortenURL := s.encode(url)
-	s.store(url, shortenURL)
+	shortenURL := fs.encode(url)
+	fs.store(url, shortenURL)
 	return shortenURL
 }
 
-func (s *Store) find(url string) (string, bool) {
-	if val, ok := s.Data[url]; ok {
-		return val, true
+func (fs *FileStore) find(url string) (string, bool) {
+	fileScanner := bufio.NewScanner(fs.File)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		splittedLine := strings.Split(line, "=")
+		log.Println(splittedLine)
+
+		if splittedLine[0] == url {
+			return splittedLine[1], true
+		}
 	}
 	return "", false
 }
 
-func (s *Store) store(key, val string) {
-	s.Data[key] = val
+func (fs *FileStore) store(key, val string) {
+	line := key + "=" + val + "\n"
+
+	n, err := fs.File.WriteString(line)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%d bytes written\n", n)
 }
 
-func (s *Store) encode(url string) string {
+func (fs *FileStore) encode(url string) string {
 	return b64.StdEncoding.EncodeToString([]byte(url))
 }
