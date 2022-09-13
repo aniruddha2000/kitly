707f688c6fffe17c1eb4f9bc138c026e4ae3d773
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-09-13 12:52:34 +0530

    Add route & handler for shortning URL
    Signed-off-by: Aniruddha Basak <codewithaniruddha@gmail.com>

--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -1,5 +1,26 @@
 package server
 
-func (s *Server) initializeRoutes()  {
-	
+import (
+	"net/http"
+
+	j "github.com/aniruddha2000/kitly/pkg/json"
+)
+
+const msg = "http://localhost:8989/"
+
+func (s *Server) initializeRoutes() {
+	s.Router.HandleFunc("/short", s.Short)
+}
+
+func (s *Server) Short(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		r.ParseForm()
+		url := r.Form.Get("url")
+
+		shortenURL := s.Shorter.Short(url)
+
+		j.JSON(w, r, http.StatusOK, msg+shortenURL)
+	} else {
+		j.JSON(w, r, http.StatusBadRequest, "Request Invalid! POST Request accepted only")
+	}
 }
